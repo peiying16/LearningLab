949e4fa14607ffc67448fdd789e441507c389dd4
peiying yang <peiying yang>
2017-04-02 17:14:29 -0400

    fix lint

--- seneca.js
+++ seneca.js
@@ -5,24 +5,24 @@
  * Given a string `email`, return `true` if the string is in the form
  * of a valid Seneca College email address, `false` othewise.
  */
-exports.isValidEmail = function (email) {
+exports.isValidEmail = function (email){
 
-  if (typeof(email) === 'undefined'){
+  if ( typeof(email) === 'undefined') {
     return false;
   }
-  if (typeof(email) !== 'string' ){
+  if ( typeof(email) !== 'string' ) {
     return false;
   }
-  if (email===''){
+  if (email==='') {
     return false;
   }
 
   //leading write space
-  if(email[0] === ' '){
+  if(email[0] === ' ') {
     return false;
   }
 
-  if(/@myseneca.ca$/.test(email) || /@senecacollege.ca$/.test(email) || /@senecac.on.ca$/.test(email)){
+  if(/@myseneca.ca$/.test(email) || /@senecacollege.ca$/.test(email) || /@senecac.on.ca$/.test(email)) {
     return true;
   }
   return false;
@@ -33,16 +33,16 @@ exports.isValidEmail = function (email) {
  * this person. NOTE: the email doesn't need to be real/valid/active.
  */
 exports.formatSenecaEmail = function (name) {
-  if (name === undefined){
+  if (name === undefined) {
     throw new Error('undefined');
   }
-  if (name === ''){
+  if (name === '') {
     return null;
   }
-  if(/ /.test(name)){
+  if(/ /.test(name)) {
     return null;
   }
-  if(/  /.test(name)){
+  if(/  /.test(name)) {
     return null;
   }
   return name.concat('@myseneca.ca');
